5627bb1c8051264469149ccbb3de5fddb046ee4b
Devesh Agale <167551610+deveshagale12@users.noreply.github.com>
2026-04-02 12:39:54 +0530

    import

--- src/main/java/com/boutique/WebConfig.java
+++ src/main/java/com/boutique/WebConfig.java
@@ -5,7 +5,10 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
